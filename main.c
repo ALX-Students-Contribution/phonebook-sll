489730d33d9f3795a2d829e58aaab047277c3d29
Thuba Tshehlane <38510942+ttshehlane@users.noreply.github.com>
2023-07-09 02:25:10 +0200

    created a basic main.c main.h file, created a stuct of contactlist that will be used for a SLL

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - phonebook main program function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct contactlist - singly linked list struct for contact details
+ * @name: full name of individual e.g. Jane Doe
+ * @phone_number: phone number as 27123456789
+ * @email_address: email address as awesome@email.com
+ * @next: pointer to the next contact detail
+ *
+ * Description: singly linked list node of contact details structure
+ *
+ */
+typedef struct contactlist
+{
+	char *name;
+	unsigned int phone_number;
+	char * email_address;
+	struct contactlist *next;
+} contact;
+
+#endif /* MAIN_H */
